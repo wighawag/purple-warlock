a4da4cc43cb9b9ada01e04009da8c436cc488813
Wighawag <wighawag@gmail.com>
2020-07-11 00:38:25 +0100

    support branch for export + export

--- toTemplate/index.js
+++ toTemplate/index.js
@@ -1,28 +1,31 @@
-const {execSync} = require('child_process');
+const { execSync } = require("child_process");
 // execSync('npm install --no-save change-case@4.1.1 fs-extra@9.0.0 tar@5.0.1')
 
-const {Transform } = require('stream');
-const tar = require('tar');
-const fs = require('fs-extra');
-const changeCase = require('change-case');
+const { Transform } = require("stream");
+const tar = require("tar");
+const fs = require("fs-extra");
+const changeCase = require("change-case");
+
+const args = process.argv.slice(2);
+const branch = args[0] || "master";
 
 const variables = {
-    name: "Purple Warlock",
-    contractName: "Gobelin Registry"
+  name: "Purple Warlock",
+  contractName: "Gobelin Registry",
 };
 
 const tests = [
-    "camelCase",
-    "constantCase",
-    "headerCase",
-    "noCase",
-    "paramCase",
-    "pascalCase",
-    "pathCase",
-    "sentenceCase",
-    "snakeCase",
-    "capitalCase",
-    "dotCase"
+  "camelCase",
+  "constantCase",
+  "headerCase",
+  "noCase",
+  "paramCase",
+  "pascalCase",
+  "pathCase",
+  "sentenceCase",
+  "snakeCase",
+  "capitalCase",
+  "dotCase",
 ];
 
 // for (const test of tests) {
@@ -32,91 +35,101 @@ const tests = [
 // }
 
 function findAndReplace(str, term, replacement) {
-    return str.split(term).join(replacement);
+  return str.split(term).join(replacement);
 }
 
 function findAndReplaceVariable(str, test, variableName) {
-    return findAndReplace(str, changeCase[test](variables[variableName]), `{{=_.${test}(it.${variableName})}}`);
+  return findAndReplace(
+    str,
+    changeCase[test](variables[variableName]),
+    `{{=_.${test}(it.${variableName})}}`
+  );
 }
 
 function findAndReplaceAll(str) {
-    str = findAndReplace(str, '}}', '{{!"}"}}}');
-    for (const variableName of Object.keys(variables)) {
-        for (const test of tests) {
-            str = findAndReplaceVariable(str, test, variableName);
-        }
+  str = findAndReplace(str, "}}", '{{!"}"}}}');
+  for (const variableName of Object.keys(variables)) {
+    for (const test of tests) {
+      str = findAndReplaceVariable(str, test, variableName);
     }
-    return str;
+  }
+  return str;
 }
 
 const transform = findAndReplaceAll;
 
-const archivePath = 'archive.tar.gz';
-const dest = 'export';
-fs.moveSync(dest + '/.git', '.git.tmp');
+const archivePath = "archive.tar.gz";
+const dest = "export";
+fs.moveSync(dest + "/.git", ".git.tmp");
 fs.removeSync(archivePath);
 fs.emptyDirSync(dest);
-const result = execSync(`git archive master -o ${archivePath}`);
+const result = execSync(`git archive ${branch} -o ${archivePath}`);
 // console.log({archiveExists: fs.existsSync(archivePath)});
 // console.log({result: result.toString()});
 
 const contents = {};
 
-console.log('extracting...', {archivePath, dest});
+console.log("extracting...", { archivePath, dest });
 try {
-    tar.x({
-        cwd: dest,
-        file : archivePath,
-        sync: true,
-        onentry(entry) {
-            entry.path = transform(entry.path);
-            return entry;
-        },
-        filter(path) {
-            console.log(path);
-            return path !== '.gitmodules' && !path.startsWith('export/') && path !== 'archive.tar.gz' && !path.startsWith('toTemplate/');
-        }, 
-        transform(entry) {
-            // console.log(entry.path);
-            // console.log(entry.bufferLength);
-            let chunks = {};
-            let counter = 0;
-            contents[entry.path] = "";
-            return entry.pipe(new Transform({
-                transform(chunk, enc, cb) {
-                    const str = chunk.toString();
-                    if (chunks[str]) {
-                        return cb();
-                    }
-                    chunks[str] = true; // WEIRD the function seems to be called 2 per chunk, checkin g duplicate chunk (NOT FULL PROOF)
-                    counter ++;
-                    // if (counter % 2 == 0) { // WEIRD the function seems to be called 2 per chunk, so we do only once per twice
-                    //     return cb();
-                    // }
-                    // if (entry.path === 'contracts/package.json') {
-                    //     console.log('CHUNK', chunk.toString());
-                    // }
-                    contents[entry.path] += chunk;
-                    // TODO streaming seatch and replace 
-                    // var upperChunk = chunk.toString().toUpperCase();
-                    // this.push(upperChunk);
-                    cb();
-                },
-                flush(cb) {
-                    const content = contents[entry.path];
-                    // if (entry.path === 'contracts/package.json') {
-                    //     console.log(content);
-                    // }
-                    // console.log('flushing ' + entry.path);
-                    this.push(transform(content));
-                    delete contents[entry.path];
-                    cb();
-                }
-            }));
-        }
-    });    
+  tar.x({
+    cwd: dest,
+    file: archivePath,
+    sync: true,
+    onentry(entry) {
+      entry.path = transform(entry.path);
+      return entry;
+    },
+    filter(path) {
+      console.log(path);
+      return (
+        path !== ".gitmodules" &&
+        !path.startsWith("export/") &&
+        path !== "archive.tar.gz" &&
+        !path.startsWith("toTemplate/")
+      );
+    },
+    transform(entry) {
+      // console.log(entry.path);
+      // console.log(entry.bufferLength);
+      let chunks = {};
+      let counter = 0;
+      contents[entry.path] = "";
+      return entry.pipe(
+        new Transform({
+          transform(chunk, enc, cb) {
+            const str = chunk.toString();
+            if (chunks[str]) {
+              return cb();
+            }
+            chunks[str] = true; // WEIRD the function seems to be called 2 per chunk, checkin g duplicate chunk (NOT FULL PROOF)
+            counter++;
+            // if (counter % 2 == 0) { // WEIRD the function seems to be called 2 per chunk, so we do only once per twice
+            //     return cb();
+            // }
+            // if (entry.path === 'contracts/package.json') {
+            //     console.log('CHUNK', chunk.toString());
+            // }
+            contents[entry.path] += chunk;
+            // TODO streaming seatch and replace
+            // var upperChunk = chunk.toString().toUpperCase();
+            // this.push(upperChunk);
+            cb();
+          },
+          flush(cb) {
+            const content = contents[entry.path];
+            // if (entry.path === 'contracts/package.json') {
+            //     console.log(content);
+            // }
+            // console.log('flushing ' + entry.path);
+            this.push(transform(content));
+            delete contents[entry.path];
+            cb();
+          },
+        })
+      );
+    },
+  });
 } finally {
-    fs.moveSync('.git.tmp', dest + '/.git');
-    fs.removeSync(archivePath);
+  fs.moveSync(".git.tmp", dest + "/.git");
+  fs.removeSync(archivePath);
 }
-
