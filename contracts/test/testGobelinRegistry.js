215d03fb7775a3804b603dbcfd05907877aa8752
Wighawag <wighawag@gmail.com>
2020-07-12 22:40:37 +0100

    better doc for HRC

--- contracts/test/testGobelinRegistry.js
+++ contracts/test/testGobelinRegistry.js
@@ -13,4 +13,10 @@ describe("GobelinRegistry", function () {
     const gobelinRegistryContract = await ethers.getContract("GobelinRegistry");
     expect(gobelinRegistryContract.fails("testing")).to.be.revertedWith("fails");
   });
+
+  it("should return 2 as id", async function () {
+    await deployments.fixture();
+    const gobelinRegistryContract = await ethers.getContract("GobelinRegistry");
+    expect(await gobelinRegistryContract.getId()).to.equal(2);
+  });
 });
