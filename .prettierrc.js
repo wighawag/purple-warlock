4b42042f1602035ccbd5b9c41f56833545834d56
Wighawag <wighawag@gmail.com>
2020-09-20 11:26:54 +0100

    add format and lint script

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,24 +1,25 @@
 module.exports = {
   semi: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   singleQuote: true,
   printWidth: 120,
   tabWidth: 2,
   useTabs: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   bracketSpacing: false,
-  arrowParens: "always",
-  pluginSearchDirs: ["./"], // required somehow (pnpm/prettier issue ?)
+  arrowParens: 'always',
+  pluginSearchDirs: ['./'], // required somehow (pnpm/prettier issue ?)
+  plugins: ['prettier-plugin-svelte', 'prettier-plugin-solidity'],
   overrides: [
     {
-      files: "*.sol",
+      files: '*.sol',
       options: {
         printWidth: 120,
         tabWidth: 4,
         singleQuote: false,
         bracketSpacing: false,
-        explicitTypes: "always"
-      }
-    }
-  ]
-}
+        explicitTypes: 'always',
+      },
+    },
+  ],
+};
