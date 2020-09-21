4e559908b3136ec0cebfacbfd1b15adb0e2ea3fb
Wighawag <wighawag@gmail.com>
2020-09-21 21:26:52 +0100

    loading bar

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -2,9 +2,15 @@
   import {Router, CurrentPage} from './_routing';
   import pages from './pages';
   import Navbar from './components/Navbar.svelte';
+  import Navigating from './_routing/curi/Navigating.svelte';
+  import Loading from './components/Loading.svelte'
 </script>
 
 <Router {pages} globalQueryStrings={['subgraph', 'debug', 'log']}>
   <Navbar links={['Home', 'Demo', 'Wallet']} />
-  <CurrentPage>Loading...</CurrentPage>
+  <CurrentPage>
+    <Loading/>
+    Loading...
+  </CurrentPage>
+  <Navigating component={Loading}/>
 </Router>
